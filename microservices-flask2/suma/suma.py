ca4ada54c52f2679199d60ea2a427472f6d81ba0
Thomas Avila <thomasdanielavila@gmail.com>
2019-09-11 15:27:22 -0500

    Add files via upload

--- microservices-flask2/suma/suma.py
+++ microservices-flask2/suma/suma.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/<num1>/<num2>")
 
-def default():
-    return "hello"
+def default(num1,num2):
+    return str(int(num1)+int(num2))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
